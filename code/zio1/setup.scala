3fce74350c3bc458f6d8ee67ca18ce928b117f6e
daniel.beskin <daniel.beskin@gmail.com>
2025-01-22 12:10:11 +0200

    Code boilerplate

---
+++ code/zio1/setup.scala
@@ -0,0 +1,3 @@
+//> using scala "3.6.2"
+//> using dep "dev.zio::zio::1.0.18"
+//> using dep "dev.zio::zio-test::1.0.18"
---
+++ code/zio2/setup.scala
@@ -0,0 +1,3 @@
+//> using scala "3.6.2"
+//> using dep "dev.zio::zio:2.1.14"
+//> using dep "dev.zio::zio-test:2.1.14"
